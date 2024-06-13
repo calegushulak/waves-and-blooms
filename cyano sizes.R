fd5cfc65dcf6ba8fe4a7e4e80558829693878df1
hdugan <hilarydugan@gmail.com>
2024-06-13 09:59:16 -0500

    1) Converted to reproducible data download 2) Check genus Chroococcus vs. Chroococcaceae

--- cyano sizes.R
+++ cyano sizes.R
@@ -2,11 +2,18 @@
 library(tidyverse)
 library(cowplot)
 
-# Download Mendota Phytoplankton data
-https://portal.edirepository.org/nis/dataviewer?packageid=knb-lter-ntl.88.31&entityid=f2de15b2fff6ae962a04c150c0a1c510
 
-### read data
-phyto <- read.csv(file.choose(), header = TRUE)
+# https://portal.edirepository.org/nis/dataviewer?packageid=knb-lter-ntl.88.31&entityid=f2de15b2fff6ae962a04c150c0a1c510
+# 
+# ### read data
+# phyto <- read.csv(file.choose(), header = TRUE)
+
+# Download Mendota Phytoplankton data
+# North Temperate Lakes LTER: Phytoplankton - Madison Lakes Area 1995 - current
+inUrl  <- "https://pasta.lternet.edu/package/data/eml/knb-lter-ntl/88/31/f2de15b2fff6ae962a04c150c0a1c510" 
+infile <- tempfile()
+download.file(inUrl,infile,method="curl")
+phyto <- read_csv(infile) 
 
 ## Restrict to Mendota cyanobacteria
 cy <- phyto %>%
@@ -18,7 +25,7 @@ cy_cat <- transform(cy, year = as.factor(year4))
 
 ### histogram of GALD sizes
 mency <- 
-  ggplot(cy, aes(gald.um))+
+  ggplot(cy, aes(gald))+
   geom_histogram(binwidth=0.1, position="identity", 
                  color="black", fill="grey")+
   scale_x_log10()+
@@ -30,7 +37,7 @@ mency
 
 ### Create boxplot of all taxa
 plot <- 
-  ggplot(cy_cat, aes(genus, gald.um))+
+  ggplot(cy_cat, aes(genus, gald))+
   geom_boxplot()+
   labs(y="log GALD um", x = "Genus")+
   scale_y_continuous(trans='log10')+
@@ -40,16 +47,19 @@ plot <-
 plot
 ggssave("")
 
-### reload data combining anabaena and dolichospermum genera as 
-## just Dolichospermum
-cytax <- read.csv(file.choose(), header=TRUE)
+### Combine anabaena and dolichospermum genera as Dolichospermum
+# cytax <- read.csv(file.choose(), header=TRUE)
+cytax = phyto |> 
+  filter(lakeid == 'ME', division == "Cyanophyta") |> 
+  mutate(genus = if_else(genus == 'Anabaena', 'Dolichospermum', genus))
 
 ## grab some key genera
 bloomers <- cytax %>%
   filter(genus > 0.0,
          genus %in% c("Dolichospermum", 
                       "Aphanizomenon",
-                      "Chroococcaceae",
+                      "Chroococcus",
+                      # "Chroococcaceae",
                       "Microcystis",
                       "Pseudanabaena",
                       "Synechococcus",
@@ -67,7 +77,7 @@ mutate(genus = case_when(genus == 'Dolichospermum' ~ 'Dolichospermumǂ',
 
 
 cy_box <- 
-  ggplot(bloomers, aes(genus, gald.um))+
+  ggplot(bloomers, aes(genus, gald))+
   geom_boxplot()+
   scale_y_log10()+
   labs(y="GALD μm", x="Taxon")+
